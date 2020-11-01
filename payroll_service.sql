3ae59c4f7cf95e098a04a83dd698b0ee58545c6c
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-01 16:18:32 +0530

    [pranav] Add. Create Database called payroll_service

--- payroll_service.sql
+++ payroll_service.sql
@@ -4,13 +4,3 @@ create database payroll_service
 use payroll_service
 
 select db_name()
-
-create table employee_payroll
-(
-id int identity(1,1),
-name varchar(25) not null,
-salary money not null,
-start date not null
-);
-
-
