998e61f8f5317455d497b8279838e412a7db8573
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-01 16:22:28 +0530

    [pranav] Add. Populate the table with records

--- payroll_service.sql
+++ payroll_service.sql
@@ -13,4 +13,5 @@ salary money not null,
 start date not null
 );
 
-
+select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'employee_payroll'
+insert into employee_payroll values('Bill', 100000.00, '2018-01-03'), ('Terisa', 200000.00, '2019-11-13'), ('Charlie', 300000.00, '2020-05-21');
